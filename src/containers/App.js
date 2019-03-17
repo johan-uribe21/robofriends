e6743bea9c75d87d5c19efafee242fe58ce0526a
Johan Uribe <Johanandresuribe@gmail.com>
2019-03-16 23:29:45 -0400

    trying to reverse clone this repo

---
+++ src/containers/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll'
+import './App.css'
+
+const state = {
+  
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchfield: '',
+    }
+  }
+
+
+  componentDidMount(){
+    // fetch users, parse the json, then update the state with the new users.
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({robots: users}));
+  } 
+
+  // use arrow function here to make sure 'this' is here, not in SearchBox input.
+  // arrow functions make "this" refer to where the function was created, not called.
+  onSearchChange = (event) => {
+    //changes state so that the searchfield gets updated as you type.
+    this.setState({ searchfield: event.target.value }); 
+  }
+  
+  render(){
+    const {robots, searchfield} = this.state;
+    const filteredRobots = robots.filter(robot =>{
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    });
+    return !robots.length ? // ternary statement
+      <h1>Loading</h1> :
+      (
+        <div className = 'tc'>
+          <h1 className = 'f2'>RoboFriends</h1>
+          <SearchBox searchChange = {this.onSearchChange}/>
+          <Scroll>
+            <CardList robots = {filteredRobots} /> 
+          </Scroll>
+        </div>
+      );
+  }
+};
+
+export default App;
