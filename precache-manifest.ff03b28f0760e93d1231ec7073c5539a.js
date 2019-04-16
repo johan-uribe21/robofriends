b251bda5c68a7a85c9e9fc59d902bf09b606031a
Johan Uribe <Johanandresuribe@gmail.com>
2019-04-16 16:45:14 -0400

    Updates

---
+++ precache-manifest.ff03b28f0760e93d1231ec7073c5539a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "47077973437d2bd0d1e1",
+    "url": "/robofriends/./static/css/main.d060dd0c.chunk.css"
+  },
+  {
+    "revision": "47077973437d2bd0d1e1",
+    "url": "/robofriends/./static/js/main.8c3c2639.chunk.js"
+  },
+  {
+    "revision": "c5c3dcb5e6834f90e8db",
+    "url": "/robofriends/./static/js/runtime~main.f743fd61.js"
+  },
+  {
+    "revision": "570c911dff70cb443ab2",
+    "url": "/robofriends/./static/css/2.8daa575c.chunk.css"
+  },
+  {
+    "revision": "570c911dff70cb443ab2",
+    "url": "/robofriends/./static/js/2.ae53b63a.chunk.js"
+  },
+  {
+    "revision": "747a91f3bf13f28f9d874f3916d0750b",
+    "url": "/robofriends/./index.html"
+  }
+];
